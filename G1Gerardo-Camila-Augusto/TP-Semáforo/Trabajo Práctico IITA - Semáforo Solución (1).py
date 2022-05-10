7bee9112b1212447951293914b2b5fa405d0ac92
Bloki2319 <sulic.augusto@gmail.com>
2022-05-09 23:43:43 -0300

    Correccion archivo

--- G1Gerardo-Camila-Augusto/TP-Semáforo/Trabajo Práctico IITA - Semáforo Solución (1).py
+++ G1Gerardo-Camila-Augusto/TP-Semáforo/Trabajo Práctico IITA - Semáforo Solución (1).py
@@ -12,13 +12,13 @@ while contadorCiclo < 4:
         ''' Si el contador es igual a 1 y su estado es rojo, se logea el estado del semáforo y se cambia posteriormente a -Amarillo-'''
         print ("Estado actual del semáforo:", estado)
         estado = "Amarillo"
-        time.sleep(0.05)
+        time.sleep(50)
     
     elif contador == 2 and estado == "Amarillo":
         ''' Si el contador es igual a 2 y su estado es amarillo, se logea el estado del semáforo y se cambia posteriormente a -Verde-'''
         print ("Estado actual del semáforo:", estado)
         estado = "Verde"
-        time.sleep(0.05)
+        time.sleep(50)
     
     elif contador == 3 and estado == "Verde":
         ''' Si el contador es igual a 1 y su estado es verde, se logea el estado del semáforo y se establece nuevamente como -Rojo-. Se agrega una unidad
@@ -28,6 +28,6 @@ while contadorCiclo < 4:
         print("- - - Ciclo número", contadorCiclo, "concluido. - - -")
         contador = 0
         estado = "Rojo"
-        time.sleep(0.05)
+        time.sleep(50)
 
 print ("Programa terminado.")
