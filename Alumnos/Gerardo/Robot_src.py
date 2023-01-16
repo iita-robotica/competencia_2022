73d50528624be1d54d26fc1a83b7c6d530a1cdc6
URGerardo <uribururomerogerardocdm@gmail.com>
2023-01-16 06:57:01 -0300

    Identificación de errores a depurar

--- Alumnos/Gerardo/Robot_src.py
+++ Alumnos/Gerardo/Robot_src.py
@@ -5,29 +5,16 @@ from controller import PositionSensor
 from controller import DistanceSensor
 import math
 
+
+#Start robot & sensors
+
 robot = Robot()
 
 timeStep = 32
-
-tilesize = 0.06 # Probar 0.10 | 0.9
-half_tilesize = tilesize/2
+robot.step(timeStep)
 
 gps = robot.getDevice("gps")
 gps.enable(timeStep)
-
-robot.step(timeStep)
-
-startX = gps.getValues()[0]/tilesize
-startY = gps.getValues()[2]/tilesize
-
-x = 0
-x1 = 0
-y = 0
-y1 = 0
-
-turn_range = 0.01
-
-encoder = 2.215 #Original 2.3
 
 ruedaIzquierda = robot.getDevice("wheel1 motor")
 ruedaIzquierda.setPosition(float('inf'))
@@ -43,22 +30,60 @@ distancia_frontal = robot.getDevice("distance sensor1")
 distancia_frontal.enable(timeStep)
 distancia_lateral = robot.getDevice("distance sensor2")
 distancia_lateral.enable(timeStep)
-distancia_lateralIzq = robot.getDevice("distance sensor3")
-distancia_lateralIzq.enable(timeStep)
 
 colorSensor = robot.getDevice("colour_sensor")
 colorSensor.enable(timeStep)
 
 
+#Start movement variables
+
+tilesize = 0.06
+tilesize_detection = tilesize/2
+
+startX = gps.getValues()[0]/tilesize
+startY = gps.getValues()[2]/tilesize
+
+turn_range = 0.01
+turn_counter = 0
+
+encoder = 2.215
 encoder_goal = encoder 
+
+x = 0
+x1 = 0
+y = 0
+y1 = 0
+
+angle = 0
+angle_permit = [0, 45, 90]
+angle_prox = 0
+
+diff_max = 0
+diff_min = 0
 
 state = "advance"
 
 counter = 0
 
-angulo = 0
 
-savedPlaces = []
+#Start mapping variables
+
+column = 0
+column_max = 0
+column_min = 0
+column_total = 0
+
+row = 0
+row_max = 0
+row_min = 0
+row_total = 0
+#rows = []
+
+point = 0
+cardinal = "north"
+
+
+#Start movement functions
 
 def advance(vx, vy):
     ruedaIzquierda.setVelocity(vx)
@@ -74,70 +99,127 @@ def get_angle():
     global x1
     global y1
     global angulo
-    if x1 != x or y1 != y:
-        angulo = math.atan2(y1-y,x1-x) * 180/math.pi
-    return angulo
+    if x1 != x and y1 != y:
+        angle = math.atan2(abs(y1-y),abs(x1-x)) * 180/math.pi
+    else:
+        angle = 0
+    return angle
 
 def angle_normalizer(ang):
-    ang = ang % 360
+    ang = ang % 90
     if ang < 0:
-        ang += 360
+        ang += 90
     return ang
 
 def danger(image):
     r = colorSensor.imageGetRed(image, 1, 0, 0)
     g = colorSensor.imageGetGreen(image, 1, 0, 0)
     b = colorSensor.imageGetBlue(image, 1, 0, 0)
-    
-    #Reconocimiento de obstáculos
-    if ( 210 <= r  <= 240) and  (180 <= g <= 210) and (100 <= b <= 130):
+    if (210 <= r <= 240) and (180 <= g <= 210) and (100 <= b <= 130):
         return True
     elif (r <= 47) and (g <= 47) and (b <= 47):
         return True
 
+def setCardinal(actualPoint):
+    global cardinal 
+    if point == 0 or point == -4:
+        cardinal = "north"
+    elif point == 1 or point == -3:
+        cardinal = "east"
+    elif point == 2 or point == -2:
+        cardinal = "south"
+    elif point == 3 or point == -1:
+        cardinal = "west"
+
 while robot.step(timeStep) != -1:
-    encoder_actual = encoderDerecho.getValue()
+    
+    #Update robot & sensors
 
     dis_frontal = distancia_frontal.getValue()
     dis_lateral = distancia_lateral.getValue()
-    dis_latIzq = distancia_lateralIzq.getValue()
-
-    # print(f'Frontal = {dis_frontal:.3f}, Derecha = {dis_lateral:.3f}, Izquierda = {dis_latIzq:.3f}')
 
     image = colorSensor.getImage()
-
+    
+    
+    #Update movement variables
+    
     x = round(gps.getValues()[0]/tilesize - startX, 1 )
     y = round(gps.getValues()[2]/tilesize - startY, 1 )
-
+    print(y)
+    
+    encoder_actual = encoderDerecho.getValue()
+    
+    if 0 <= angle < 30:
+        angle_prox = 0
+    elif 30 <= angle <= 60:
+        angle_prox = 45
+    elif 60 < angle <= 90:
+        angle_prox = 90
+        
+    diff_max = angle + 12
+    diff_min = angle - 12
+    
     counter += 1
+    
+    #Update mapping variables
+    
+    #if column_max < column:
+        #column_max = column
+    #elif column < column_min:
+        #column_min = column
+    #column_total = abs(column_min) + column_max
+        
+    #if row_max < row:
+        #row_max = row
+    #elif row < row_min:
+        #row_min = row
+    #row_total = abs(row_min) + row_max
+    
+    if (4 <= point) or (point <= -5):
+        point = 0
+    #Initialize state machine
 
     if counter % 110 == 0 :
-        #print(f'Valores de la posicion inicial y final: (x, x1 , y, y1) : {x}, {x1}, {y}, {y1}')
         get_angle()
-        #x1 = x
-        #y1 = y
         counter %= 5
 
     if state == "advance":
-        # print(angulo)
-        if angulo not in [0, 45, 90, 135, 180, 215, 270, 315]:
-            advance(1,1)
-            #valor_proximo = min([0, 90, 180, 270], key=angulo)
-            #print(f'Valor mas proximo a mi angulo deseado: {valor_proximo}')
+        setCardinal(point)
+        if angle not in angle_permit:
+            state = "advance_fix"
+            if (diff_max <= angle_prox) or (angle_prox <= diff_min):
+                if turn_counter == 20:
+                    turn_counter = 0
+                    advance(1,0.97)
+                else:
+                    advance(1,1)
         else:
             advance(1,1)
             
-        if dis_frontal < half_tilesize or danger(image):
+        if dis_frontal < tilesize_detection or danger(image):
             state = "turn"
-            if dis_lateral < half_tilesize:
+            if dis_lateral < tilesize_detection:
                 encoder_goal = encoder_actual - encoder
+                turn_counter += 1
+                point -= 1
                 turn(-0.5)
-                savedPlaces.append((x,y))
             else:
                 encoder_goal = encoder_actual + encoder
+                turn_counter += 1
+                point += 1
                 turn(0.5)
-
+        
     elif state == "turn":
+        angle = 0
         if(abs(encoder_actual - encoder_goal) < turn_range):
             state = "advance"
             encoder_actual = 45
+    
+    elif state == "advance_fix":
+        if (angle_prox < diff_max) or (diff_min < angle_prox):
+            state = "advance"
+            angle = 0
+
+#Arreglar el sistema de GPS (No detecta valores de posición)
+#determinar cada cuanos valores del gps se avanza una casilla
+#norte o este suma uno a la columa o fila, sur u este resta uno a la columna o fila
